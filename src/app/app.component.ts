7b18bec94367e85f29128875d9d81b2278e69a51
Alexander Dause <alexander.dause@web.de>
2023-11-08 22:30:48 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg'];
-  currentImage = 0;
-  showImage = true;
-
-
-  ngOnInit() {
-    this.slideImages();
-  }
-
-
-  slideImages() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-      setTimeout(() => {
-        this.showImage = true;
-      }, 1);
-    }, 8000);
-  }
-
+export class AppComponent {
 
 }
