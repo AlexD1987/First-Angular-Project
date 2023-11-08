b5ce858905c9ffeda4b727eed755149d94e4d912
Alexander Dause <alexander.dause@web.de>
2023-11-08 15:44:17 +0100

    add slideShow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'firts_angular_project';
+export class AppComponent implements OnInit {
+  images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+
+  ngOnInit() {
+    this.slideImages();
+  }
+
+
+  slideImages() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 1);
+    }, 8000);
+  }
+
+
 }
