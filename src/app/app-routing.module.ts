e5574355a316f883ceca44348035d451ededccce
Alexander Dause <alexander.dause@web.de>
2023-11-09 11:27:25 +0100

    add header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StoriesComponent } from './stories/stories.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { PlacesComponent } from './places/places.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SlideshowComponent},
+  { path: 'stories', component: StoriesComponent},
+  { path: 'places', component: PlacesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { PlacesComponent } from './places/places.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SlideshowComponent
+    SlideshowComponent,
+    HeaderComponent,
+    StoriesComponent,
+    PlacesComponent
   ],
   imports: [
     BrowserModule,
